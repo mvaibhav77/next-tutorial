3d479dfd7066bcfbdf7da243e7b745f857860d55
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-11-17 12:39:54 +0530

    deployment ready

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://vaibhav-next-tutorial.vercel.app";
+  : "https://next-tutorial-ncnh9uu6f-mvaibhav77.vercel.app";
