b0b909e2dafef51e44183fa3710a7b7cd986db7c
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-11-17 12:45:05 +0530

    domain change

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-tutorial-ncnh9uu6f-mvaibhav77.vercel.app";
+  : "https://vs-next-tutorial.vercel.app.vercel.app";
